bf568768df27472a95b6ef5f9903590bf4763f95
Lennart ten Wolde <48725857+lennarttenwolde@users.noreply.github.com>
2026-03-10 19:04:42 +1300

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-
-import preact from '@astrojs/preact';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tenwolde.dev',
-  integrations: [preact(), sitemap()],
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
   }
